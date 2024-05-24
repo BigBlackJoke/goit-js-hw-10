3652a4488bc67e6f1a79b4b4456847a912d0ceb5
BigBlackJoke <marisha.zorina@gmail.com>
2024-05-24 20:24:10 +0300

    almost

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,10 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const button = document.querySelector("button");
+const inputMs = document.querySelector('input[name="delay"]');
+const radioFulfilled = document.querySelector('input[name="state"][value="fulfilled"]');
+const radioRejected = document.querySelector('input[name="state"][value="rejected"]');
+
+
+
