695e436747b5d07dad0601547c0d32b48007a7ec
BigBlackJoke <marisha.zorina@gmail.com>
2024-05-25 12:57:21 +0300

    finish

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,32 +1,38 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const button = document.querySelector("button");
+const form = document.querySelector(".form");
 const inputMs = document.querySelector('input[name="delay"]');
 const radioFulfilled = document.querySelector('input[name="state"][value="fulfilled"]');
 const radioRejected = document.querySelector('input[name="state"][value="rejected"]');
 
-button.addEventListener("submit", () => {
-    setTimeout(makeNewPromise, inputMs.value);
+form.addEventListener("submit", event => {
+    event.preventDefault();
     const makeNewPromise = () => {
-        switch (radioFulfilled, radioRejected) {
-            case radioFulfilled = true:
-                Promise.resolve(iziToast.show({
-                    message: 'Fulfilled promise in ${inputMs.value}ms',
-                    position: 'topCenter'
-                }));
-                break;
-            case radioRejected = true:
-                Promise.reject(iziToast.show({
-                    message: 'Rejected promise in ${inputMs.value}ms',
-                    position: 'topCenter'
-                }));
-                break;
-            default:
+        if (radioFulfilled.checked) {
+            Promise.resolve().then(() => {
                 iziToast.show({
-                    message: 'Something went wrong...',
-                    position: 'topCenter'
-                });
-       }
+                    message: `Fulfilled promise in ${inputMs.value}ms`,
+                    position: 'topCenter',
+                    messageColor: 'rgba(255, 255, 255, 1)',
+                    messageSize: '16px',
+                    backgroundColor: 'rgba(89, 161, 13, 1)',
+                    close: false,
+                    timeout: 1500
+                })
+            });
+        } else if (radioRejected.checked) {
+            Promise.reject().catch(() => iziToast.show({
+                    message: `Rejected promise in ${inputMs.value}ms`,
+                    position: 'topCenter',
+                    messageColor: 'rgba(255, 255, 255, 1)',
+                    messageSize: '16px',
+                    backgroundColor: 'rgba(181, 27, 27, 1)',
+                    close: false,
+                    timeout: 1500
+                }));
+        }
     };
+    setTimeout(makeNewPromise, inputMs.value);
 });
+
