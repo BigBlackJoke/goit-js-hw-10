9d63c406f6f010654f5578fefc12fee42fdb92be
BigBlackJoke <marisha.zorina@gmail.com>
2024-05-25 11:33:20 +0300

    reload

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,5 +6,27 @@ const inputMs = document.querySelector('input[name="delay"]');
 const radioFulfilled = document.querySelector('input[name="state"][value="fulfilled"]');
 const radioRejected = document.querySelector('input[name="state"][value="rejected"]');
 
-
-
+button.addEventListener("submit", () => {
+    setTimeout(makeNewPromise, inputMs.value);
+    const makeNewPromise = () => {
+        switch (radioFulfilled, radioRejected) {
+            case radioFulfilled = true:
+                Promise.resolve(iziToast.show({
+                    message: 'Fulfilled promise in ${inputMs.value}ms',
+                    position: 'topCenter'
+                }));
+                break;
+            case radioRejected = true:
+                Promise.reject(iziToast.show({
+                    message: 'Rejected promise in ${inputMs.value}ms',
+                    position: 'topCenter'
+                }));
+                break;
+            default:
+                iziToast.show({
+                    message: 'Something went wrong...',
+                    position: 'topCenter'
+                });
+       }
+    };
+});
